0721278314ab8fd63d868efaee2f066ca618b63e
Ryan Coble <ryancoble77@gmail.com>
2017-01-02 20:44:32 -0600

    added ssdp lookup for roku devices on the network

--- roku.py
+++ roku.py
@@ -1,13 +1,6 @@
-
 import requests
 
-
-## initialize and constants
-roku_ip = '192.168.1.133'
-roku_port = '8060'
-rokuUrl = 'http://'+ roku_ip +':'+ roku_port
-
-
+##
 Channels = {
     'Netflix': '12',
     'Amazon': '13',
@@ -46,37 +39,32 @@ Keys = {
 
 #function definitions
 def getApps(url):
-    response = requests.get(url+ '/query/apps')
+    response = requests.get(url+ 'query/apps')
     responseString = response.content
     return responseString
 
 
 def getActiveApp(url):
-    response = requests.get(url+ '/query/active-app')
+    response = requests.get(url+ 'query/active-app')
     responseString = response.content
     return responseString
 
 
 def launchAppByID(url, appID):
-    response = requests.post(url+ '/launch/'+ appID)
+    response = requests.post(url+ 'launch/'+ appID)
     responseString = response.content
     return responseString
 
 
 def searchContent(url, keyword, type, provider):
-    response = requests.post(url+ '/search/browse?keyword='+ keyword +'&type='+ type +'&provider-id='+ provider +'&launch=true')
+    response = requests.post(url+ 'search/browse?keyword='+ keyword +'&type='+ type +'&provider-id='+ provider +'&launch=true')
     responseString = response.content
     return responseString
 
 
 def keyPress(url, key):
-    response = requests.post(url+ '/keypress/'+ key)
+    response = requests.post(url+ 'keypress/'+ key)
     responseString = response.content
     return responseString
-
-
-
-
-
 
 
--- server.py
+++ server.py
@@ -1,68 +1,76 @@
 from flask import Flask
 from flask import redirect
 from flask import url_for
+import ssdp
 import roku
 import os
 
 # Initialize, read app configuration and setup
 app = Flask(__name__)
 port = int(os.getenv('VCAP_APP_PORT', 8080))
+try:
+    print "finding roku..."
+    rokuUrl = str(ssdp.discover("roku:ecp"))
+    print "roku found at: "+ rokuUrl
+except:
+    print "Error getting finding roku... setting to default"
+    rokuUrl = "http://192.168.1.133:8060/"
 
 
 # keys
 @app.route('/home')
 def home():
-    returnString = roku.keyPress(roku.rokuUrl, roku.Keys['Home'])
+    returnString = roku.keyPress(rokuUrl, roku.Keys['Home'])
     return redirect(url_for('status'))
 
 
 @app.route('/play')
 def play():
-    returnString = roku.keyPress(roku.rokuUrl, roku.Keys['Play'])
+    returnString = roku.keyPress(rokuUrl, roku.Keys['Play'])
     return redirect(url_for('status'))
 
 
 @app.route('/back')
 def back():
-    returnString = roku.keyPress(roku.rokuUrl, roku.Keys['Back'])
+    returnString = roku.keyPress(rokuUrl, roku.Keys['Back'])
     return redirect(url_for('status'))
 
 
 @app.route('/enter')
 def enter():
-    returnString = roku.keyPress(roku.rokuUrl, roku.Keys['Enter'])
+    returnString = roku.keyPress(rokuUrl, roku.Keys['Enter'])
     return redirect(url_for('status'))
 
 
 @app.route('/select')
 def select():
-    returnString = roku.keyPress(roku.rokuUrl, roku.Keys['Select'])
+    returnString = roku.keyPress(rokuUrl, roku.Keys['Select'])
     return redirect(url_for('status'))
 
 
 @app.route('/info')
 def info():
-    returnString = roku.keyPress(roku.rokuUrl, roku.Keys['Info'])
+    returnString = roku.keyPress(rokuUrl, roku.Keys['Info'])
     return redirect(url_for('status'))
 
 @app.route('/right')
 def right():
-    returnString = roku.keyPress(roku.rokuUrl, roku.Keys['Right'])
+    returnString = roku.keyPress(rokuUrl, roku.Keys['Right'])
     return redirect(url_for('status'))
 
 @app.route('/left')
 def left():
-    returnString = roku.keyPress(roku.rokuUrl, roku.Keys['Left'])
+    returnString = roku.keyPress(rokuUrl, roku.Keys['Left'])
     return redirect(url_for('status'))
 
 @app.route('/up')
 def up():
-    returnString = roku.keyPress(roku.rokuUrl, roku.Keys['Up'])
+    returnString = roku.keyPress(rokuUrl, roku.Keys['Up'])
     return redirect(url_for('status'))
 
 @app.route('/down')
 def down():
-    returnString = roku.keyPress(roku.rokuUrl, roku.Keys['Down'])
+    returnString = roku.keyPress(rokuUrl, roku.Keys['Down'])
     return redirect(url_for('status'))
 
 
@@ -71,19 +79,19 @@ def down():
 # launch apps
 @app.route('/Netflix')
 def Netflix():
-    roku.launchAppByID(roku.rokuUrl, roku.Channels['Netflix'])
+    roku.launchAppByID(rokuUrl, roku.Channels['Netflix'])
     return redirect(url_for('status'))
 
 
 @app.route('/Amazon')
 def Amazon():
-    roku.launchAppByID(roku.rokuUrl, roku.Channels['Amazon'])
+    roku.launchAppByID(rokuUrl, roku.Channels['Amazon'])
     return redirect(url_for('status'))
 
 
 @app.route('/HBO')
 def HBO():
-    roku.launchAppByID(roku.rokuUrl, roku.Channels['HBO_GO'])
+    roku.launchAppByID(rokuUrl, roku.Channels['HBO_GO'])
     return redirect(url_for('status'))
 
 
@@ -92,33 +100,34 @@ def HBO():
 # search
 @app.route('/searchNetflix/<string>')
 def searchNetflix(string):
-    roku.searchContent(roku.rokuUrl, string, roku.ContentType['Movie'], roku.Channels['Netflix'])
+    roku.searchContent(rokuUrl, string, roku.ContentType['Movie'], roku.Channels['Netflix'])
     return redirect(url_for('status'))
 
 
 @app.route('/searchAmazon/<string>')
 def searchAmazon(string):
-    roku.searchContent(roku.rokuUrl, string, roku.ContentType['Movie'], roku.Channels['Amazon'])
+    roku.searchContent(rokuUrl, string, roku.ContentType['Movie'], roku.Channels['Amazon'])
     return redirect(url_for('status'))
 
 
 @app.route('/searchHBO/<string>')
 def searchHBO(string):
-    roku.searchContent(roku.rokuUrl, string, roku.ContentType['Movie'], roku.Channels['HBO_GO'])
+    roku.searchContent(rokuUrl, string, roku.ContentType['Movie'], roku.Channels['HBO_GO'])
     return redirect(url_for('status'))
 
 
 # Info
 @app.route('/')
 def status():
-    returnString = roku.getActiveApp(roku.rokuUrl)
+    print "RokuURL: "+ rokuUrl
+    returnString = roku.getActiveApp(rokuUrl)
     return str(returnString)
 
 
 # Info
 @app.route('/apps')
 def apps():
-    returnString = roku.getApps(roku.rokuUrl)
+    returnString = roku.getApps(rokuUrl)
     return str(returnString)
 
 
